f3ec3e57cf18df8eed2833afce9fa2c61ea3d2ab
Jennifer <du.jennifer1@gmail.com>
2020-12-19 18:37:43 -0800

    Initial Commit

---
+++ Study Buddies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Study Buddies
+//
+//  Created by Jennifer Du on 12/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
