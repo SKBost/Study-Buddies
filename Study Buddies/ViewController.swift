154041d9b73ee665a710bd46c8c6075f0214bf46
Jennifer <du.jennifer1@gmail.com>
2020-12-21 18:44:37 -0800

    login page

--- Study Buddies/ViewController.swift
+++ Study Buddies/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newUserButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newUserButton.layer.cornerRadius = 5.0
+        logInButton.layer.cornerRadius = 5.0
+        
     }
 
 
