72a3aeca569913bc3171f6ce129a3752195b1590
Doctor167 <59671332+Doctor167@users.noreply.github.com>
2021-01-04 02:11:13 +0800

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ruby
+//
+//  Created by mac on 12/30/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var button: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        button.layer.cornerRadius = 20
+        Button2.layer.cornerRadius = 20
+        
+    }
+
+
+}
+
